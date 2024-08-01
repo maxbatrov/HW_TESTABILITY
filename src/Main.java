9d7e770c45d8d2481fb43d0cb84079380c6ee44e
Max Batrov <maxbatrov@gmail.com>
2024-08-01 18:45:41 +0200

    Result

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 56890;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
